3a3edcfc89ae3078582160895a749624a3dfc66e
Griffin Fujioka <fujiokag@hotmail.com>
2012-09-27 03:00:46 -0700

    first commit - PoC
    This commit contains a proof of concept. I thought this would be a cool
    idea, so I looked into a few things and am now set on publishing it.
    
    Found a nice demo for Windows Phone showing how to simulate snow fall,
    brought that over the Windows 8 and added some libraries and it works
    the same. I'm happy about that and must remember to reference the demo I
    found - I believe it was a Windows Phone Geek tutorial.
    
    I determined that I'm able to get today's date, subtract it from date of
    christmas and figure out how many days and hours are left until
    Christmas.
    
    TODO:
    Format the time span difference as Days, Hours, Minutes and Seconds
    until Christmas.
    
    Add references and credits.
    
    Artwork

---
+++ MainPage.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Windows.Foundation;
+using System.Windows;
+using System.Windows.Input; 
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;       // ellipse
+using Windows.UI.Xaml.Media.Animation;  // storyboard
+using Windows.UI.Xaml.Media;
+using Windows.UI;           // colors
+using Windows.Globalization.DateTimeFormatting; // datetime formating
+
+/*
+ * I'm basically porting this app over from a Windows Phone 7 tutorial 
+ * and adding some new features, namely a date difference calculation, 
+ * and calling it a Christmas Countdown. I've never ported from Windows Phone 7 
+ * to Windows 8 before and am in the process of determining if I can go through 
+ * and just add the missing libraries to make this thing work. 
+ */
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ChristmasCountdown
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region On Navigated To
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+        #endregion 
+        
+        private static Random random;
+
+        #region Constructor
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            random = new Random();
+            this.StartFallingSnowAnimation();
+
+            // We keep results in this variable
+            StringBuilder results = new StringBuilder();
+            results.AppendLine();
+
+            // Create basic date/time formatters.
+            DateTimeFormatter[] basicFormatters = new[]
+            {
+                // Default date formatters
+                new DateTimeFormatter("shortdate"),
+                new DateTimeFormatter("longdate"),
+
+                // Default time formatters
+                new DateTimeFormatter("shorttime"),
+                new DateTimeFormatter("longtime"),
+             };
+
+            // Create date/time to format, manipulate and display.
+            DateTime dateTime = DateTime.Now;
+            // Try to format and display date/time if calendar supports it.
+            foreach (DateTimeFormatter formatter in basicFormatters)
+            {
+                try
+                {
+                    // Format and display date/time.
+                    results.AppendLine(formatter.Template + ": " + formatter.Format(dateTime));
+                }
+                catch (ArgumentException)
+                {
+                    // Retrieve and display formatter properties. 
+                    results.AppendLine(String.Format(
+                        "Unable to format Gregorian DateTime {0} using formatter with template {1} for languages [{2}], region {3}, calendar {4} and clock {5}",
+                        dateTime,
+                        formatter.Template,
+                        string.Join(",", formatter.Languages),
+                        formatter.GeographicRegion,
+                        formatter.Calendar,
+                        formatter.Clock));
+                }
+            }
+
+            // Display the results
+            this.todaysDateTxtBlock.Text = results.ToString();
+
+            DateTime Christmas = new DateTime(DateTime.Today.Year, 12, 25);
+            TimeSpan ts = Christmas - DateTime.Now;
+            int days = ts.Days;
+            int hours = ts.Hours;
+            diffTxtBlock.Text = days + " days " + hours + "hours"; 
+        }
+
+        
+        #endregion 
+
+        #region Start the snow fall
+        private void StartFallingSnowAnimation()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Ellipse localCopy = this.GenerateEllipse();
+                localCopy.SetValue(Canvas.LeftProperty, i * 30 + 1.0);
+
+                double y = Canvas.GetTop(localCopy);
+                double x = Canvas.GetLeft(localCopy);
+
+                double speed = 5 * random.NextDouble();
+                double index = 0;
+                double radius = 30 * speed * random.NextDouble();
+
+                localCopy.Opacity = .3 + random.NextDouble();
+
+                CompositionTarget.Rendering += delegate(object o, object arg)
+                {
+                    index += Math.PI / (180 * speed);
+
+                    if (y < this.ContentPanel.DesiredSize.Height)
+                    {
+                        y += .3 + speed;
+                    }
+                    else
+                    {
+                        y = -localCopy.Height;
+                    }
+
+                    Canvas.SetTop(localCopy, y);
+                    Canvas.SetLeft(localCopy, x + radius * Math.Cos(index));
+                    Storyboard animation = this.CreateStoryboard(localCopy, y, x + radius * Math.Cos(index));
+                    Storyboard.SetTarget(animation, localCopy);
+
+                    animation.Begin();
+
+                };
+            }
+        }
+        #endregion 
+
+        #region Generate an Ellipse
+        private Ellipse GenerateEllipse()
+        {
+            Ellipse element = new Ellipse();
+            element.Fill = new SolidColorBrush(Colors.White);
+            element.Height = 10.0;
+            element.Width = 10.0;
+            this.ContentPanel.Children.Add(element);
+            return element;
+        }
+        #endregion 
+
+        #region Create storyboard
+        private Storyboard CreateStoryboard(UIElement element, double to, double toLeft)
+        {
+            Storyboard result = new Storyboard();
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.To = to;
+            //Storyboard.SetTargetProperty(animation, new PropertyPath("(Canvas.Top)"));
+            Storyboard.SetTargetProperty(animation, "(Canvas.Top)");
+            Storyboard.SetTarget(animation, element);
+
+            DoubleAnimation animationLeft = new DoubleAnimation();
+            animationLeft.To = toLeft;
+            //Storyboard.SetTargetProperty(animationLeft, new PropertyPath("(Canvas.Left)"));
+            Storyboard.SetTargetProperty(animationLeft, "(Canvas.Left)"); 
+            Storyboard.SetTarget(animationLeft, element);
+
+            result.Children.Add(animation);
+            result.Children.Add(animationLeft);
+
+            return result;
+        }
+        #endregion 
+    }
+}
